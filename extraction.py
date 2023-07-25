1563b60c0f696a35bed95657f902be1168a39603
Rekha Varupula <138374966+Rekha-Varupula@users.noreply.github.com>
2023-07-25 12:33:22 +0530

    Update extraction.py

--- extraction.py
+++ extraction.py
@@ -3,5 +3,5 @@
 import pandas as pd
 
 def extract_data(file_path):
-    df = pd.read_csv(file_path, encoding=('ISO-8859-1'), index_col=0)
+    df = pd.read_csv(file_path, encoding=('ISO-8859-1'))
     return df
